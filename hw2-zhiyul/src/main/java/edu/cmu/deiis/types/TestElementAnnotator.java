a526ab897801a17f854498b0145cfeaa146a6876
Zhiyu Li <golzygo@gmail.com>
2013-09-21 11:01:04 -0400

    added stanford-nlp dependencies, TestElementAnnotator done.

--- hw2-zhiyul/src/main/java/edu/cmu/deiis/types/TestElementAnnotator.java
+++ hw2-zhiyul/src/main/java/edu/cmu/deiis/types/TestElementAnnotator.java
@@ -3,6 +3,7 @@ package edu.cmu.deiis.types;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.jcas.JCas;
 import edu.cmu.deiis.types.Question;
+import edu.cmu.deiis.types.Answer;
 
 public class TestElementAnnotator extends JCasAnnotator_ImplBase{
   public void process(JCas aJCas){
@@ -15,10 +16,26 @@ public class TestElementAnnotator extends JCasAnnotator_ImplBase{
         // found one question - create annotation
         Question annotation=new Question(aJCas);
         annotation.setBegin(linePosition+2);
-        annotation.setEnd(linePosition+line.length()+2); // +2 for the "\n"
-        linePosition+=(line.length()+3); // +2, then +1 to the beginning of next line
+        annotation.setEnd(linePosition+line.length());
+        linePosition+=(line.length()+1); // +1 to the beginning of next line
         annotation.setCasProcessorId("edu.cmu.deiis.types.TestElementAnnotator");
         annotation.setConfidence(1.0);
+        annotation.addToIndexes();
+      }
+      else if(line.startsWith("A")){
+        // found one answer - create annotation
+        Answer annotation=new Answer(aJCas);
+        annotation.setBegin(linePosition+4);
+        annotation.setEnd(linePosition+line.length());
+        linePosition+=(line.length()+1); // +1 to the beginning of next line
+        annotation.setCasProcessorId("edu.cmu.deiis.types.TestElementAnnotator");
+        annotation.setConfidence(1.0);
+        if(line.charAt(2)=='1'){
+          annotation.setIsCorrect(true);
+        }
+        else{
+          annotation.setIsCorrect(false);
+        }
         annotation.addToIndexes();
       }
     }
